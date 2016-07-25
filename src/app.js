e9372ce57c3fb53456c1bc0f02f4f0256e0f9ea0
Francesco Zanini <francesco@zanini.me>
2016-07-25 16:17:27 +0200

    Fix muiThemeProvider error
    Change-Id: I850b0ff5ab4ca3ef12075b87c7691ef8a1a258c0

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ import App from 'containers/App/App'
 import {hashHistory} from 'react-router'
 import makeRoutes from './routes'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 const initialState = {}
 import {configureStore} from './redux/configureStore'
 const {store, actions, history} = configureStore({initialState, historyType: hashHistory});
@@ -19,11 +21,13 @@ let render = (routerKey = null) => {
 
   const mountNode = document.querySelector('#root');
   ReactDOM.render(
-    <App history={history}
-          store={store}
-          actions={actions}
-          routes={routes}
-          routerKey={routerKey} />, mountNode);
+    <MuiThemeProvider>
+      <App history={history}
+            store={store}
+            actions={actions}
+            routes={routes}
+            routerKey={routerKey} />
+    </MuiThemeProvider>, mountNode);
 }
 
 if (__DEBUG__ && module.hot) {
