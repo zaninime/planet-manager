06403b167ae2e1512baff84896829db88c39e01c
Francesco Zanini <francesco@zanini.me>
2016-07-26 16:43:13 +0200

    Fix touch tap events
    Per Material UI guide http://www.material-ui.com/#/get-started/installation
    
    Change-Id: I0187e9d1978e4ee35422f2ce9fb7d5bcbfbd545f

--- src/app.js
+++ src/app.js
@@ -10,6 +10,11 @@ import {hashHistory} from 'react-router'
 import makeRoutes from './routes'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const initialState = {}
 import {configureStore} from './redux/configureStore'
