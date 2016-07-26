21dbe97c968f467007c3970eee3b55eeb4ec9248
Francesco Zanini <francesco@zanini.me>
2016-07-26 18:46:49 +0200

    Create BoundChannelColor
    Change-Id: I34b82d9ede1db89d2ec50658225225e1cc9dec12

---
+++ src/containers/advanced/BoundChannelColor/BoundChannelColor.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import ChannelColor from 'components/advanced/ChannelColor/ChannelColor';
+
+const getCurrentColor(state, stripNumber) {
+  const COLORS = {
+    1: 'white'
+    2: 'red'
+    3: 'green'
+    4: 'blue'
+    5: 'off'
+  };
+  return COLORS[state[stripNumber]];
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  color: getCurrentColor(state.lampConfig.channelMapping, ownProps.number)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onClick: dispatch(/* toggleColor */)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChannelColor);
