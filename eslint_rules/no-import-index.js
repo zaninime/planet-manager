107785c9eede4d31e6a1eb3fd02cf77fc1545965
Francesco Zanini <francesco@zanini.me>
2016-09-21 14:39:40 +0200

    Add custom ESLint rules
    Change-Id: Id9eea06a4019b4a8c1849d14acde72bfe8f3857e

---
+++ eslint_rules/no-import-index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  create(context) {
+    return {
+      ImportDeclaration(node) {
+        const pattern = /(.*)\/index(?:.js)?$/;
+        const match = node.source.value.match(pattern);
+        if (match !== null) {
+          context.report({node: node.source, message: `'${node.source.value}' is not allowed. Use '${match[1]}' instead`});
+        }
+      }
+    };
+  }
+};
