31a9425995a459a9a4cb61efe811b5732faf8287
Francesco Zanini <francesco@zanini.me>
2016-10-07 23:33:28 +0200

    Update, strip and check-in third-party dependencies
    Change-Id: I4083a013be9be06845e85630befc5697dd1fa972

--- android.webpack.js
+++ android.webpack.js
@@ -1,5 +1,4 @@
 const NODE_ENV = process.env.NODE_ENV || 'development';
-const dotenv = require('dotenv');
 
 const webpack = require('webpack');
 const path = require('path');
@@ -10,20 +9,17 @@ const resolve = path.resolve;
 const getConfig = require('hjs-webpack');
 
 const isDev = NODE_ENV === 'development';
-const isTest = NODE_ENV === 'test';
 
 // devServer config
 const devHost   = process.env.HOST || '0.0.0.0';
 const devPort   = process.env.PORT || 3001;
 
-//const setPublicPath = process.env.SET_PUBLIC_PATH !== 'false';
 //const publicPath  = (isDev && setPublicPath) ? `//${devHost}:${devPort}/` : '';
 const publicPath = '';
 
 const root = resolve(__dirname);
 const src = join(root, 'src');
-const modules = join(root, 'node_modules');
-const dest = join(root, 'dist', 'android');
+const dest = join(root, 'dist', 'ios');
 //const css = join(src, 'styles');
 
 var config = getConfig({
@@ -33,7 +29,7 @@ var config = getConfig({
   html: function (context) {
     return {
       'index.html': context.defaultTemplate({
-        title: 'planet-manager',
+        title: 'Planet Manager',
         publicPath,
         meta: {}
       })
@@ -42,76 +38,25 @@ var config = getConfig({
 });
 
 config.output.publicPath = '';
+config.resolve.root = ['src'];
 
-config.resolve.extensions.push(".android.js", ".coffee");
+config.resolve.extensions.push('.android.js');
 
-// ENV variables
-const environmentEnv = dotenv.config({
-  path: join(root, 'config', `${NODE_ENV}.config.js`),
-  silent: true
-});
-const envVariables =
-    Object.assign({}, environmentEnv);
-
-const defines =
-  Object.keys(envVariables)
-  .reduce((memo, key) => {
-    const val = JSON.stringify(envVariables[key]);
-    memo[`__${key.toUpperCase()}__`] = val;
-    return memo;
-  }, {
-    __NODE_ENV__: JSON.stringify(NODE_ENV),
-    __DEBUG__: isDev
-  });
+const defines = {
+  __NODE_ENV__: JSON.stringify(NODE_ENV),
+  __DEBUG__: isDev
+};
 
 config.plugins = [
   new webpack.DefinePlugin(defines)
 ].concat(config.plugins);
-// END ENV variables
 
-// Roots
-config.resolve.root = [src, modules];
-config.resolve.alias = {
-  actions: join(src, 'actions'),
-  reducers: join(src, 'reducers'),
-  css: join(src, 'styles'),
-  components: join(src, 'components'),
-  containers: join(src, 'containers'),
-  math: join(src, 'math'),
-  protocol: join(src, 'protocol'),
-  utils: join(src, 'utils'),
-  styles: join(src, 'styles')
-};
-// end Roots
+config.resolve.root = [src];
 
 // Dev
 if (isDev) {
   config.devServer.port = devPort;
   config.devServer.hostname = devHost;
 }
-
-// Testing
-if (isTest) {
-  config.externals = {
-    'react/addons': true,
-    'react/lib/ReactContext': true,
-    'react/lib/ExecutionEnvironment': true
-  };
-
-  config.module.noParse = /[/\\]sinon\.js/;
-  config.resolve.alias.sinon = 'sinon/pkg/sinon';
-
-  config.plugins = config.plugins.filter(p => {
-    const name = p.constructor.toString();
-    const fnName = name.match(/^function (.*)\((.*\))/);
-
-    const idx = [
-      'DedupePlugin',
-      'UglifyJsPlugin'
-    ].indexOf(fnName[1]);
-    return idx < 0;
-  });
-}
-// End Testing
 
 module.exports = config;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const NODE_ENV = process.env.NODE_ENV || 'development';
-const dotenv = require('dotenv');
 
 const webpack = require('webpack');
 const path = require('path');
@@ -10,7 +9,6 @@ const resolve = path.resolve;
 const getConfig = require('hjs-webpack');
 
 const isDev = NODE_ENV === 'development';
-const isTest = NODE_ENV === 'test';
 
 // devServer config
 const devHost   = process.env.HOST || '0.0.0.0';
@@ -21,9 +19,7 @@ const publicPath  = (isDev && setPublicPath) ? `//${devHost}:${devPort}/` : '';
 
 const root = resolve(__dirname);
 const src = join(root, 'src');
-const modules = join(root, 'node_modules');
 const dest = join(root, 'dist', 'electron');
-//const css = join(src, 'styles');
 const fs = require('fs');
 
 // load files for Electron app
@@ -50,117 +46,25 @@ var config = getConfig({
 });
 
 config.output.publicPath = '';
-config.resolve.modulesDirectories = ['src', 'node_modules'];
+config.resolve.root = ['src'];
 
-config.resolve.extensions.push(".desktop.js", ".coffee");
+config.resolve.extensions.push(".desktop.js");
 
-// ENV variables
-const environmentEnv = dotenv.config({
-  path: join(root, 'config', `${NODE_ENV}.config.js`),
-  silent: true
-});
-const envVariables =
-    Object.assign({}, environmentEnv);
-
-const defines =
-  Object.keys(envVariables)
-  .reduce((memo, key) => {
-    const val = JSON.stringify(envVariables[key]);
-    memo[`__${key.toUpperCase()}__`] = val;
-    return memo;
-  }, {
-    __NODE_ENV__: JSON.stringify(NODE_ENV),
-    __DEBUG__: isDev
-  });
+const defines = {
+  __NODE_ENV__: JSON.stringify(NODE_ENV),
+  __DEBUG__: isDev
+};
 
 config.plugins = [
   new webpack.DefinePlugin(defines)
 ].concat(config.plugins);
-// END ENV variables
 
-// CSS modules
-const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
-
-const matchCssLoaders = /(^|!)(css-loader)($|!)/;
-
-const findLoader = (loaders, match) => {
-  const found = loaders.filter(l => l && l.loader && l.loader.match(match));
-  return found ? found[0] : null;
-};
-
-// existing css loader
-const cssloader =
-  findLoader(config.module.loaders, matchCssLoaders);
-
-const newloader = Object.assign({}, cssloader, {
-  test: /\.module\.css$/,
-  include: [src],
-  loader: cssloader.loader.replace(matchCssLoaders, `$1$2?modules&localIdentName=${cssModulesNames}$3`)
-});
-config.module.loaders.push(newloader);
-cssloader.test = new RegExp(`[^module]${cssloader.test.source}`);
-cssloader.loader = newloader.loader;
-
-config.module.loaders.push({
-  test: /\.css$/,
-  include: [modules],
-  loader: 'style!css'
-});
-
-// CSS modules
-
-// postcss
-config.postcss = [].concat([
-  require('precss')({}),
-  require('autoprefixer')({}),
-  require('cssnano')({})
-]);
-
-// END postcss
-
-// Roots
-config.resolve.root = [src, modules];
-config.resolve.alias = {
-  actions: join(src, 'actions'),
-  reducers: join(src, 'reducers'),
-  css: join(src, 'styles'),
-  components: join(src, 'components'),
-  containers: join(src, 'containers'),
-  math: join(src, 'math'),
-  //protocol: join(src, 'protocol'),
-  utils: join(src, 'utils'),
-  styles: join(src, 'styles')
-};
-// end Roots
+config.resolve.root = [src];
 
 // Dev
 if (isDev) {
   config.devServer.port = devPort;
   config.devServer.hostname = devHost;
 }
-
-// Testing
-if (isTest) {
-  config.externals = {
-    'react/addons': true,
-    'react/lib/ReactContext': true,
-    'react/lib/ExecutionEnvironment': true
-  };
-
-  config.module.noParse = /[/\\]sinon\.js/;
-  config.resolve.alias.sinon = 'sinon/pkg/sinon';
-
-  config.plugins = config.plugins.filter(p => {
-    const name = p.constructor.toString();
-    const fnName = name.match(/^function (.*)\((.*\))/);
-
-    const idx = [
-      'DedupePlugin',
-      'UglifyJsPlugin'
-    ].indexOf(fnName[1]);
-    return idx < 0;
-  });
-}
-// End Testing
 
 module.exports = config;
