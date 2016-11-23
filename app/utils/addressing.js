6fd10ac16453528b3ee7670b90d3d24e0cc3ba76
Omar Mahili <omar.mahili@gmail.com>
2016-11-23 19:31:54 +0100

    Fix wifi config defaults

--- app/utils/addressing.js
+++ app/utils/addressing.js
@@ -49,11 +49,11 @@ export const wifiToProtocolFormat = (wifiState) => {
     const { mode } = wifiState;
     const { ssid } = wifiState[mode];
     let password = '';
-    let dhcp = false;
+    let dhcp = true;
     let addressing = { };
-    let ipAddress = '';
-    let netmask = '';
-    let gateway = '';
+    let ipAddress = '192.168.0.100';
+    let netmask = '255.255.255.0';
+    let gateway = '192.168.0.1';
 
     // No need to validate them since the state
     // must never contain invalid values.
