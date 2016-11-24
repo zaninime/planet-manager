27beb901d6ee2480f6d77711b66c5c61c4b48a63
Francesco Zanini <francesco@zanini.me>
2016-11-24 16:33:08 +0100

    Disable FlowType - ESLint integration (#12)
    The integration has been disabled mainly because of Flow bugs. It's impossible to have a clean build without incurring some errors like "required module not found" or "missing property in object literal".
    
    By merging this, the CI will no longer try to run Flow as a part of the build but it becomes a responsibility of the developer(s) to check for Flow errors (and all the usual things).
    
    To make life easier, npm run flow is provided.

--- app/protocol/photon/emitter.js
+++ app/protocol/photon/emitter.js
@@ -4,6 +4,8 @@ import { twilightDuration, floorIntensity, compactChannels } from './constants';
 import { supportOnSave } from './bugs';
 import type { HighLevelConfig, Features } from './types';
 
+type Caps = { features: Features, bugs: string[] };
+
 const emitTarget = target => (config, caps) =>
    Object.keys(target).reduce((nextTarget, key) => {
        const result = nextTarget;
@@ -95,7 +97,7 @@ export const daylight = (config: HighLevelConfig) => {
     };
 };
 
-export const channels = (config: HighLevelConfig, { features }) => {
+export const channels = (config: HighLevelConfig, { features }: Caps) => {
     const convert = (ch) => {
         if (!ch.enabled) return 'off';
         return ch.color;
@@ -114,7 +116,7 @@ export const channels = (config: HighLevelConfig, { features }) => {
     return config.channels.map(convert);
 };
 
-export const temperature = (config, { features }) => {
+export const temperature = (config: HighLevelConfig, { features }: Caps) => {
     if (!features.TEMPERATURE_CONFIG) {
         return {
             fanStart: 0,
@@ -124,7 +126,7 @@ export const temperature = (config, { features }) => {
     return { ...config.temperature };
 };
 
-export const fan = (config, { features }) => {
+export const fan = (config: HighLevelConfig, { features }: Caps) => {
     if (!features.FAN_CONFIG) {
         return {
             minSpeed: 0,
@@ -135,12 +137,12 @@ export const fan = (config, { features }) => {
     return { ...config.fan };
 };
 
-export const night = config => ({
+export const night = (config: HighLevelConfig) => ({
     color: config.night.color,
     intensity: Math.round(config.night.intensity * 100),
 });
 
-export const mode = config => (config.master ? 'master' : 'slave');
+export const mode = (config: HighLevelConfig) => (config.master ? 'master' : 'slave');
 
 export const emitDemo = daylightColor;
 
